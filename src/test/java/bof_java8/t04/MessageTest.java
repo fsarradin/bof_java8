2bc3b602e0bb8e0007609760f8f819999ad87a95
François Sarradin <fsarradin@gmail.com>
2012-05-06 23:40:50 +0200

    add MessageTest to test VEM

---
+++ src/test/java/bof_java8/t04/MessageTest.java
@@ -0,0 +1,34 @@
+package bof_java8.t04;
+
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class MessageTest {
+
+    @Test
+    public void should_get_message() {
+        Message message = new MessageImpl("Hello");
+
+        assertThat(message.getMessage()).isEqualTo("Hello");
+//        assertThat(message.getPrefixedMessage()).isEqualTo("It says: Hello");
+    }
+
+    private static interface Message {
+        String getMessage();
+    }
+
+    private static class MessageImpl implements Message {
+        private final String message;
+
+        public MessageImpl(String message) {
+            this.message = message;
+        }
+
+        @Override
+        public String getMessage() {
+            return message;
+        }
+    }
+
+}
