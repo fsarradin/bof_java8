65ce441c12e06e0c97507c7108967009ef82b601
François Sarradin <fsarradin@gmail.com>
2012-09-21 00:15:04 +0200

    fake mixin from Brian Goetz

---
+++ src/test/java/bof_java8/FakeBrokenMixinTest.java
@@ -0,0 +1,41 @@
+package bof_java8;
+
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.WeakHashMap;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class FakeBrokenMixinTest {
+
+    public static interface FakeBrokenMixin {
+
+        static Map<FakeBrokenMixin, String> NAMES = Collections.synchronizedMap(new WeakHashMap<FakeBrokenMixin, String>());
+
+        String getName() default { return NAMES.get(this); }
+        void setName(String name) default { NAMES.put(this, name); }
+
+    }
+
+    public static interface MyInterface extends FakeBrokenMixin, Runnable {}
+
+    private static MyInterface newMyInterface() {
+        return () -> { System.out.println("X"); };
+    }
+
+    @Test
+    public void should_get_different_names() {
+        MyInterface x1 = newMyInterface();
+        MyInterface x2 = newMyInterface();
+
+        x1.setName("x1");
+        x2.setName("x2");
+
+        assertThat(x1).isNotEqualTo(x2);
+
+//        assertThat(x1.getName()).isNotEqualTo(x2.getName());
+    }
+
+}
