f87b91b2fb59dd5273471a0a96bc99eb8cebc28e
François Sarradin <fsarradin@gmail.com>
2012-09-21 00:18:54 +0200

    workaround due to a bug in java 8

--- src/main/java/bof_java8/PhoneCoder.java
+++ src/main/java/bof_java8/PhoneCoder.java
@@ -1,13 +1,13 @@
 package bof_java8;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.google.common.annotations.VisibleForTesting;
+
+import java.util.*;
+import java.util.functions.Factory;
 
 public class PhoneCoder {
 
-    private static final Map<Character, String> mnemonics = new HashMap<Character, String>() {{
+    private static final Map<Character, String> MNEMONICS = new HashMap<Character, String>() {{
         put('2', "ABC");
         put('3', "DEF");
         put('4', "GHI");
@@ -18,13 +18,24 @@ public class PhoneCoder {
         put('9', "WXYZ");
     }};
 
-    private static final Map<Character, Character> charCode = new HashMap<Character, Character>() {{
-        mnemonics.forEach((digit, letters)->{
-            letters.asChars().forEach(letter->{
-                put(letter, digit);
+    private static final Map<Character, Character> CHAR_CODE = getCharCode(MNEMONICS);
+    private static Map<Character, Character> getCharCode(Map<Character, String> mnemonics) {
+        Map<Character, Character> charCode = new HashMap<>();
+        mnemonics.forEach((digit, letters) -> {
+            letters.asChars().forEach(letter -> {
+                charCode.put(letter, digit);
             });
         });
-    }};
+        return Collections.unmodifiableMap(charCode);
+    }
+
+//    private static final Map<Character, Character> charCode = new HashMap<Character, Character>() {{
+//        MNEMONICS.forEach((digit, letters) -> {
+//            letters.asChars().forEach(letter -> {
+//                put(letter, digit);
+//            });
+//        });
+//    }};
 
     private final Map<String, Set<String>> numberForWords;
 
@@ -36,16 +47,17 @@ public class PhoneCoder {
         return numberForWords.get(number);
     }
 
-    public static String wordCode(String word) {
-        return (String) word.toUpperCase().asChars()
-                .map(letter->charCode.get(letter).toString())
-                .reduce("", (number, digit)->number + digit);
+    @VisibleForTesting
+    protected static String wordCode(String word) {
+        return word.toUpperCase().asChars()
+                .map(letter -> CHAR_CODE.get(letter).toString())
+                .reduce("", (number, digit) -> number + digit);
     }
 
     public static Map<String, Set<String>> distributeWords(Set<String> words) {
         Map<String, Set<String>> map = new HashMap<>();
 
-        words.forEach(word->{
+        words.forEach(word -> {
             String number = wordCode(word);
             if (!map.containsKey(number)) {
                 map.put(number, new HashSet<String>());
