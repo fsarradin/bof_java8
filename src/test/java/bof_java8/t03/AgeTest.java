85ab069850c1ffbb843cb8912953101476c21e15
François Sarradin <fsarradin@gmail.com>
2012-09-29 23:27:40 +0200

    add test for java 8 version

--- src/test/java/bof_java8/t03/AgeTest.java
+++ src/test/java/bof_java8/t03/AgeTest.java
@@ -17,56 +17,6 @@ import static org.fest.assertions.Assertions.assertThat;
  */
 public class AgeTest {
 
-    @Test
-    public void should_distributed_ages() {
-        Iterable<Person> persons = Arrays.asList(
-                new Person("Bob", 30),
-                new Person("Brice", 10),
-                new Person("Alexander", 1)
-        );
-
-        Map<DevelopmentStage, ? extends Iterable<Person>> personDistribution;
-
-        personDistribution = distribute_1(persons);
-
-        assertThat(personDistribution.get(DevelopmentStage.INFANT)).satisfies(containsOnlyPersonWithName("Alexander"));
-        assertThat(personDistribution.get(DevelopmentStage.CHILD)).satisfies(containsOnlyPersonWithName("Brice"));
-        assertThat(personDistribution.get(DevelopmentStage.ADULT)).satisfies(containsOnlyPersonWithName("Bob"));
-    }
-
-    private Map<DevelopmentStage, ? extends Iterable<Person>> distribute_1(Iterable<Person> persons) {
-        Map<DevelopmentStage, Collection<Person>> distribution = new HashMap<>();
-
-        for (Person person : persons) {
-            DevelopmentStage development = getDevelopmentStage(person);
-
-            if (!distribution.containsKey(development)) {
-                distribution.put(development, new ArrayList<Person>());
-            }
-            distribution.get(development).add(person);
-        }
-
-        return distribution;
-    }
-
-    private static DevelopmentStage getDevelopmentStage(Person person) {
-        DevelopmentStage development;
-
-        if (person.getAge() <= 1) {
-            development = DevelopmentStage.INFANT;
-        } else if (person.getAge() <= 12) {
-            development = DevelopmentStage.CHILD;
-        } else if (person.getAge() <= 19) {
-            development = DevelopmentStage.ADOLESCENT;
-        } else if (person.getAge() <= 59) {
-            development = DevelopmentStage.ADULT;
-        } else {
-            development = DevelopmentStage.SENIOR;
-        }
-
-        return development;
-    }
-
     private static class Person {
 
         private final String name;
@@ -93,6 +43,85 @@ public class AgeTest {
         ADOLESCENT,
         ADULT,
         SENIOR;
+    }
+
+    private static DevelopmentStage getDevelopmentStage(Person person) {
+        DevelopmentStage development;
+
+        if (person.getAge() <= 1) {
+            development = DevelopmentStage.INFANT;
+        } else if (person.getAge() <= 12) {
+            development = DevelopmentStage.CHILD;
+        } else if (person.getAge() <= 19) {
+            development = DevelopmentStage.ADOLESCENT;
+        } else if (person.getAge() <= 59) {
+            development = DevelopmentStage.ADULT;
+        } else {
+            development = DevelopmentStage.SENIOR;
+        }
+
+        return development;
+    }
+
+    /* IMPERATIVE SOLUTION */
+
+    @Test
+    public void should_distributed_ages_imperative_way() {
+        Iterable<Person> persons = Arrays.asList(
+                new Person("Bob", 30),
+                new Person("Brice", 10),
+                new Person("Alexander", 1)
+        );
+
+        Map<DevelopmentStage, ? extends Iterable<Person>> personDistribution
+                = distribute_imperative(persons);
+
+        assertThat(personDistribution.get(DevelopmentStage.INFANT))
+                .satisfies(containsOnlyPersonWithName("Alexander"));
+        assertThat(personDistribution.get(DevelopmentStage.CHILD))
+                .satisfies(containsOnlyPersonWithName("Brice"));
+        assertThat(personDistribution.get(DevelopmentStage.ADULT))
+                .satisfies(containsOnlyPersonWithName("Bob"));
+    }
+
+    private Map<DevelopmentStage, ? extends Iterable<Person>> distribute_imperative(Iterable<Person> persons) {
+        Map<DevelopmentStage, Collection<Person>> distribution = new HashMap<>();
+
+        for (Person person : persons) {
+            DevelopmentStage development = getDevelopmentStage(person);
+
+            if (!distribution.containsKey(development)) {
+                distribution.put(development, new ArrayList<Person>());
+            }
+            distribution.get(development).add(person);
+        }
+
+        return distribution;
+    }
+
+    /* JAVA 8 SOLUTION */
+
+    @Test
+    public void should_distributed_ages_java8_way() {
+        Iterable<Person> persons = Arrays.asList(
+                new Person("Bob", 30),
+                new Person("Brice", 10),
+                new Person("Alexander", 1)
+        );
+
+        Map<DevelopmentStage, ? extends Iterable<Person>> personDistribution
+                = distribute_java8(persons);
+
+        assertThat(personDistribution.get(DevelopmentStage.INFANT))
+                .satisfies(containsOnlyPersonWithName("Alexander"));
+        assertThat(personDistribution.get(DevelopmentStage.CHILD))
+                .satisfies(containsOnlyPersonWithName("Brice"));
+        assertThat(personDistribution.get(DevelopmentStage.ADULT))
+                .satisfies(containsOnlyPersonWithName("Bob"));
+    }
+
+    private Map<DevelopmentStage, ? extends Iterable<Person>> distribute_java8(Iterable<Person> persons) {
+        return null;
     }
 
     private Condition<Iterator<?>> containsOnlyPersonWithName(final String name) {
