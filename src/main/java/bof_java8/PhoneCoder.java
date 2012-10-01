f26d4dcca18447b700243e5ef4f3ad0c4400609f
François Sarradin <fsarradin@gmail.com>
2012-10-01 23:05:55 +0200

    phone coder ready to demo

--- src/main/java/bof_java8/PhoneCoder.java
+++ src/main/java/bof_java8/PhoneCoder.java
@@ -1,7 +1,5 @@
 package bof_java8;
 
-import com.google.common.annotations.VisibleForTesting;
-
 import java.util.*;
 
 public class PhoneCoder {
@@ -17,55 +15,23 @@ public class PhoneCoder {
         put('9', "WXYZ");
     }};
 
-    private static final Map<Character, Character> LETTER_TO_DIGIT = getCharCode();
-    private static Map<Character, Character> getCharCode() {
-        Map<Character, Character> charCode = new HashMap<>();
-        MNEMONICS.forEach((digit, letters) -> {
-            letters.asChars().forEach(letter -> {
-                charCode.put(letter, digit);
-            });
-        });
-        return Collections.unmodifiableMap(charCode);
+    public PhoneCoder(Set<String> words) {
     }
 
-//    private static final Map<Character, Character> charCode = new HashMap<Character, Character>() {{
-//        MNEMONICS.forEach((digit, letters) -> {
-//            letters.asChars().forEach(letter -> {
-//                put(letter, digit);
-//            });
-//        });
-//    }};
+    public static Map<Character, Character> charToDigit(Map<Character, String> mnemonics) {
+        return null;
+    }
 
-    private final Map<String, Set<String>> numberToWord;
+    public static String getNumberFrom(String word) {
+        return null;
+    }
 
-    public PhoneCoder(Set<String> words) {
-        this.numberToWord = distributeWords(words);
+    public static Map<String, Set<String>> distributeWords(Set<String> words) {
+        return null;
     }
 
     public Set<String> translate(String number) {
-        return numberToWord.get(number);
-    }
-
-    @VisibleForTesting
-    public static String getNumberFrom(String word) {
-        return word.toUpperCase().asChars()
-                .map(letter -> LETTER_TO_DIGIT.get(letter).toString())
-                .reduce("", (number, digit) -> number + digit);
-    }
-
-    @VisibleForTesting
-    public static Map<String, Set<String>> distributeWords(Set<String> words) {
-        Map<String, Set<String>> distributedWords = new HashMap<>();
-
-        words.forEach(word -> {
-            String number = getNumberFrom(word);
-            if (!distributedWords.containsKey(number)) {
-                distributedWords.put(number, new HashSet<String>());
-            }
-            distributedWords.get(number).add(word.toUpperCase());
-        });
-
-        return distributedWords;
+        return null;
     }
 
 }
--- src/test/java/bof_java8/t05/PhoneCoderTest.java
+++ src/test/java/bof_java8/t05/PhoneCoderTest.java
@@ -1,21 +1,32 @@
 package bof_java8.t05;
 
 import bof_java8.PhoneCoder;
-import org.fest.assertions.Assertions;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.fest.assertions.api.Assertions.entry;
+
 
 public class PhoneCoderTest {
 
     @Test
+    public void should_get_2_when_A_B_or_C() {
+        HashMap<Character, String> mnemonics = new HashMap<Character, String>() {{
+            put('2', "ABC");
+        }};
+
+        Map<Character, Character> charCode = PhoneCoder.charToDigit(mnemonics);
+
+        assertThat(charCode)
+                .hasSize(3)
+                .contains(entry('A', '2'), entry('B', '2'), entry('C', '2'));
+    }
+
+    @Test
     public void should_get_5282_when_JAVA() {
-        Assertions.assertThat(PhoneCoder.getNumberFrom("Java")).isEqualTo("5282");
+        assertThat(PhoneCoder.getNumberFrom("Java")).isEqualTo("5282");
     }
 
     @Test
