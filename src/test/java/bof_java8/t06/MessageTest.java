973969d12b84ea3805625fde870c38b63b3d9a62
François Sarradin <fsarradin@gmail.com>
2012-10-02 16:13:36 +0200

    refactor

--- src/test/java/bof_java8/t06/MessageTest.java
+++ src/test/java/bof_java8/t06/MessageTest.java
@@ -6,19 +6,27 @@ import static org.fest.assertions.api.Assertions.assertThat;
 
 public class MessageTest {
 
+    public interface Message {
+        String getMessage();
+    }
+
     @Test
     public void should_get_message() {
-        Message message = new MessageImpl("Hello");
+        Message message = buildMessage("Hello");
 
         assertThat(message.getMessage()).isEqualTo("Hello");
 //        assertThat(message.getPrefixedMessage()).isEqualTo("It says: Hello");
     }
 
-    private static interface Message {
-        String getMessage();
+    /*
+     * You cannot access to what is below!!!
+     */
+
+    private Message buildMessage(String message) {
+        return new MessageImpl(message);
     }
 
-    private static final class MessageImpl implements Message {
+    private final class MessageImpl implements Message {
         private final String message;
 
         public MessageImpl(String message) {
