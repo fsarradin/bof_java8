d776ea486c18ff75aee330e4826cdd0e717e1e6b
François Sarradin <fsarradin@gmail.com>
2012-10-02 11:31:32 +0200

    add sand box test container

---
+++ src/test/java/bof_java8/SandboxTest.java
@@ -0,0 +1,15 @@
+package bof_java8;
+
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class SandboxTest {
+
+    @Test
+    public void should_do_something() {
+
+        assertThat(true).isTrue();
+    }
+
+}
